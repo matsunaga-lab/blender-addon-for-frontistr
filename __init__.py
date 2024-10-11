e945970ecbd39fd70e653c066ab7ca869820b8b9
Takuya Matsunaga <matsunaga@mps.q.t.u-tokyo.ac.jp>
2024-10-11 20:55:43 +0900

    workaround for vtk import error on linux and mac

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "Add-on for FrontISTR",
     "author": "Takuya Matsunaga",
-    "version": (0, 0, 3),
+    "version": (0, 0, 5),
     "blender": (4, 2, 0),
     "location": "File > Import > FrontISTR",
     "description": "Import and visualize FrontISTR files",
@@ -22,6 +22,29 @@ if True: # True | False
 		except ImportError:
 			subprocess.run([sys.executable, "-m", "pip", "install", module])
 
+# workaround for vtk import error on linux and mac
+if True: # True | False
+    import os
+    if os.name == "posix":
+        import site
+        for dir in site.getsitepackages():
+            filepath0 = os.path.join(dir,"vtk.py")
+            filepath1 = os.path.join(dir,"vtk_bak.py")
+            if not os.path.isfile(filepath0):
+                continue
+            if "blender-addon-for-frontistr" in open(filepath0).read():
+                continue
+            if os.path.isfile(filepath1):
+                continue
+            with open(filepath0,"r") as f0, open(filepath1,"w") as f1:
+                f1.write(f0.read())
+            with open(filepath0,"w") as f0, open(filepath1,"r") as f1:
+                for line in f1:
+                    if "from vtkmodules.vtkRenderingMatplotlib import *" in line:
+                        f0.write("# Commented out for workaround for VTK import error on linux by blender-addon-for-frontistr\n")
+                        f0.write("# "+line)
+                    else:
+                        f0.write(line)
 
 if "bpy" in locals():
     import importlib
